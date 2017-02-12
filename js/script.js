112347a67de82c9dbe164263d4b1454c322f76c0
Renaldas <renaldas.pocevicius@gmail.com>
2017-02-12 21:37:54 +0000

    add.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function (){
+
+	$("#navbarToggle").blur(function(event){
+		var screenWidth = window.innerWidth;
+		if(screenWidth < 768){
+			$("#collapsable-nav").collapse('hide');
+
+		}
+	});
+	$("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+  });
+});
