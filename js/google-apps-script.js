924acbecb3539599c1b0af717ef125d6ff2bd841
Renaldas <project.renaldas@gmail.com>
2017-02-16 12:31:07 +0000

    add

--- js/google-apps-script.js
+++ js/google-apps-script.js
@@ -4,7 +4,7 @@
  * All credit still goes to Martin and any issues/complaints/questions to me. *
  ******************************************************************************/
 
-var TO_ADDRESS = "project.renaldas@gmail.com"; // change this ...
+var TO_ADDRESS = "kjell@advokat-myrland.no"; // change this ...
 
 function formatMailBody(obj) { // function to spit out all the keys/values from the form in HTML
   var result = "";
