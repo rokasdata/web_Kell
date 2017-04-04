650d6738dab63db0bcf73e1840802d9096e26289
Renaldas <project.renaldas@gmail.com>
2017-04-04 10:04:25 +0100

    add

--- js/ajax-utils.js
+++ js/ajax-utils.js
@@ -1,36 +1,67 @@
-(function(global){
+(function (global) {
 
-	var ajaxUtils ={};
+// Set up a namespace for our utility
+var ajaxUtils = {};
 
-	//returns an HTTP request object
-	function getRequestObject() {
-		if (window.XMLHttpRequest){
-			return(new XMLHttpRequest());
-		}
-		else if (window.ActiveXObject){
-			return( new AtiveXOject("Microsoft.XMLHTTP"));
-		}
-		else{
-			global.alert("Ajax is not supprted!");
-			return(null);
 
-		}
-	}
-	//Makes an Ajax Get request to 'requestURL'
-	ajaxUtils.getSendRequest =
-		function handleResponse(requestUrl, responseHandler, isJasonResponse){
-			if((request.readyState ==4) && (request.status ==200)) {
-				if(isJasonResponse == undefined){
-					isJasonResponse = true;
-				}
-				if(isJasonResponse){
-					responseHandler(JSON.parse(request.responseText));
-				}
-				else{
-					responseHandler(request.responseText);
-				}
-			}
-		}
+// Returns an HTTP request object
+function getRequestObject() {
+  if (window.XMLHttpRequest) {
+    return (new XMLHttpRequest());
+  } 
+  else if (window.ActiveXObject) {
+    // For very old IE browsers (optional)
+    return (new ActiveXObject("Microsoft.XMLHTTP"));
+  } 
+  else {
+    global.alert("Ajax is not supported!");
+    return(null); 
+  }
+}
 
-		global.$ajaxUtils = ajaxUtils;
+
+// Makes an Ajax GET request to 'requestUrl'
+ajaxUtils.sendGetRequest = 
+  function(requestUrl, responseHandler, isJsonResponse) {
+    var request = getRequestObject();
+    request.onreadystatechange = 
+      function() { 
+        handleResponse(request, 
+                       responseHandler,
+                       isJsonResponse); 
+      };
+    request.open("GET", requestUrl, true);
+    request.send(null); // for POST only
+  };
+
+
+// Only calls user provided 'responseHandler'
+// function if response is ready
+// and not an error
+function handleResponse(request,
+                        responseHandler,
+                        isJsonResponse) {
+  if ((request.readyState == 4) &&
+     (request.status == 200)) {
+
+    // Default to isJsonResponse = true
+    if (isJsonResponse == undefined) {
+      isJsonResponse = true;
+    }
+
+    if (isJsonResponse) {
+      responseHandler(JSON.parse(request.responseText));
+    }
+    else {
+      responseHandler(request.responseText);
+    }
+  }
+}
+
+
+// Expose utility to the global object
+global.$ajaxUtils = ajaxUtils;
+
+
 })(window);
+
--- js/script.js
+++ js/script.js
@@ -1,29 +1,34 @@
-$(function (){
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
-	$("#navbarToggle").blur(function(event){
-		var screenWidth = window.innerWidth;
-		if(screenWidth < 768){
-			$("#collapsable-nav").collapse('hide');
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
 
-		}
-	});
-	$("#navbarToggle").click(function (event) {
+  // In Firefox and Safari, the click event doesn't retain the focus
+  // on the clicked button. Therefore, the blur event will not fire on
+  // user clicking somewhere else in the page and the blur event handler
+  // which is set up above will not be called.
+  // Refer to issue #28 in the repo.
+  // Solution: force focus on the element that the click event fired on
+  $("#navbarToggle").click(function (event) {
     $(event.target).focus();
   });
 });
+
 (function (global) {
 
 var dc = {};
 
-// var homeHtmlUrl = "snippets/home-snippet.html";
-// var allCategoriesUrl =
-//   "https://davids-restaurant.herokuapp.com/categories.json";
-// var categoriesTitleHtml = "snippets/categories-title-snippet.html";
-// var categoryHtml = "snippets/category-snippet.html";
-// var menuItemsUrl =
-//   "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
-// var menuItemsTitleHtml = "snippets/menu-items-title.html";
-// var menuItemHtml = "snippets/menu-item.html";
+var homeHtml = "snippets/snippet-home.html";
+var aboutHtml = "snippets/snippet-about.html";
+var contactUsHtml = "snippets/snippet-contactUs.html";
+var practiceHtml = "snippets/snippet-practice.html";
+var customersHtml = "snippets/snippet-customers.html";
+var activityHtml = "snippets/snippet-activity.html";
 
 // Convenience function for inserting innerHTML for 'select'
 var insertHtml = function (selector, html) {
@@ -36,6 +41,85 @@ var showLoading = function (selector) {
   var html = "<div class='text-center'>";
   html += "<img src='images/ajax-loader.gif'></div>";
   insertHtml(selector, html);
-
 };
+var hideMe = function (selector){
+	var html = document.querySelector("#p_1");
+	html.classList.add("hide-me");
+	insertHtml(selector)
+};
+
+// On page load (before images or CSS)
+document.addEventListener("DOMContentLoaded", function (event) {
+
+// On first load, show home view
+showLoading("#main-insertHtml");
+$ajaxUtils.sendGetRequest(
+  homeHtml,
+  function (responseText) {
+    document.querySelector("#main-insertHtml")
+      .innerHTML = responseText;
+  },
+  false);
+
+dc.loadAboutHtml = function(){
+	showLoading("#main-insertHtml");
+$ajaxUtils.sendGetRequest(
+  aboutHtml,
+  function (responseText) {
+    document.querySelector("#main-insertHtml")
+      .innerHTML = responseText;
+  },
+  false);
+};
+
+dc.loadContactUsHtml = function(){
+	hideMe("#main-insertHtml");
+	showLoading("#main-insertHtml");
+
+$ajaxUtils.sendGetRequest(
+  contactUsHtml,
+  function (responseText) {
+    document.querySelector("#main-insertHtml")
+      .innerHTML = responseText;
+
+  },
+  false);
+};
+dc.loadActivityHtml = function(){
+
+	showLoading("#main-insertHtml");
+$ajaxUtils.sendGetRequest(
+  activityHtml,
+  function (responseText) {
+    document.querySelector("#main-insertHtml")
+      .innerHTML = responseText;
+  },
+  false);
+};
+dc.loadCustomersHtml = function(){
+	showLoading("#main-insertHtml");
+$ajaxUtils.sendGetRequest(
+  customersHtml,
+  function (responseText) {
+    document.querySelector("#main-insertHtml")
+      .innerHTML = responseText;
+  },
+  false);
+};
+dc.loadPracticeHtml = function(){
+	showLoading("#main-insertHtml");
+$ajaxUtils.sendGetRequest(
+  practiceHtml,
+  function (responseText) {
+    document.querySelector("#main-insertHtml")
+      .innerHTML = responseText;
+  },
+  false);
+};
+
+});
+
+
+global.$dc = dc;
+
 })(window);
